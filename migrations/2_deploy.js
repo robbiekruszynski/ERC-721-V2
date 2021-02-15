4628f8ab37df81a865f57d02a962d8f3cd41e214
robbie <robbiekruszynski@gmail.com>
2021-02-15 10:36:36 -0800

    update metadata pathing in migration

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -3,5 +3,5 @@
 const ERC721PresetMinterPauserAutoId = artifacts.require("ERC721PresetMinterPauserAutoId");
 
 module.exports = function(deployer) {
-  deployer.deploy(ERC721PresetMinterPauserAutoId, "DevRel token","DEVREL", "https://my-json-server.typicode.com/abcoathup/samplenft/tokens/");
+  deployer.deploy(ERC721PresetMinterPauserAutoId, "DevRel token","DEVREL", "https://my-json-server.typicode.com/robbiekruszynski/devrel-721-metadata/tokens/");
 };
